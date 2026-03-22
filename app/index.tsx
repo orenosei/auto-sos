dc2a2ae3d2cd3d365a485cda82c3810f368f073a
Minh Thanh Trinh <thanh.tm.dev@gmail.com>
2026-03-22 21:39:40 +0700

    reboot

--- app/index.tsx
+++
@@ -1,7 +0,0 @@
-import { Redirect } from "expo-router";
-
-function Home() {
-  return <Redirect href='/(auth)/welcome' />
-}
-
-export default Home
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: ["./App.tsx", "./components/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
